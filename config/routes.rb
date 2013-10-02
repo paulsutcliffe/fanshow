61a837520f5d99505b8f85dfc4ee2758af67fea7
Paul Sutcliffe <paul@kosmyka.com>
2013-10-01 20:25:40 -0500

    facebook callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Fanshow::Application.routes.draw do
   resources :temas
 
-  devise_for :fans, :path => "fans", :path_names => { :sign_in => 'ingresar', :sign_out => 'salir', :password => 'secreto', :confirmation => 'verificacion', :unlock => 'desbloquear', :registration => 'registro', :sign_up => 'inscribirse' }
+  devise_for :fans, :controllers => { :omniauth_callbacks => "fans/omniauth_callbacks" }, :path => "fans", :path_names => { :sign_in => 'ingresar', :sign_out => 'salir', :password => 'secreto', :confirmation => 'verificacion', :unlock => 'desbloquear', :registration => 'registro', :sign_up => 'inscribirse' }
 
   resources :fans do
     member do
