c64e95680659341b203dc99a07dc1ef1137a28a9
Ricardo <marcorcp1@hotmail.com>
2013-10-02 01:31:54 -0500

    cleaning code

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -5,30 +5,24 @@
 // 
 // });
 
-console.log('working');
 
 $(document).ready(function() {
 
 function check_validate() {
 
-  console.log('funct works');  
   checkboxes_checked = $(':checkbox:checked').length;
-  max_check = 3;
+  max_check = 20;
   
   if (checkboxes_checked == max_check) {
-    console.log('works');
     $(':checkbox:not(:checked)').attr('disabled', 'disabled'); 
   } else {
     checkboxes.removeAttr('disabled');
   }
 }
 
-console.log('working 2');
 checkboxes = $(':checkbox');
 checkboxes.change(function() {
-  console.log('click a');
   check_validate();
-  console.log('click b');
 });
 
 });
